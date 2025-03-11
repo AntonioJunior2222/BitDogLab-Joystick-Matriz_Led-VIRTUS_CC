7d3b60d3e3f96cad18f1e6d8ff89c4a271e5ba44
Antonio Roberto <arpljunior1991@hotmail.com>
2025-03-11 20:29:10 -0300

    Create hub

---
+++ joystick.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "hardware/interp.h"
+#include "hardware/timer.h"
+#include "hardware/watchdog.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
+#include "hardware/adc.h"
+#include "libs/joystick_lib.h"
+#include "libs/malha_led.h"
+#include "libs/direcoes.h"
+
+
+#define botao_1 5
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS   17
+#define PIN_SCK  18
+#define PIN_MOSI 19
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+// Data will be copied from src to dst
+const char src[] = "Hello, world! (from DMA)";
+char dst[count_of(src)];
+
+#include "blink.pio.h"
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+
+    printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+}
+
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    // Put your timeout handler code in here
+    return 0;
+}
+
+
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+//Microfone
+const int mic = 28; // MIC
+const int ADC_CHANNEL_2 = 2;
+
+
+void config_mic()
+{
+    adc_init(); // Iniciando o ADC para os eixos
+    adc_gpio_init(mic); // Configurando o pino 28
+}
+
+void mic_leitura(uint16_t *microfone)
+{
+
+    adc_select_input(ADC_CHANNEL_2);
+    sleep_us(2);
+    *microfone = adc_read();
+}
+
+
+
+int main()
+{
+    stdio_init_all();
+    init_joystick();
+    npInit();
+    npClear();
+    npWrite(); // Escreve os dados nos LEDs.
+    config_mic();
+    //uint16_t valor_mic;
+
+    while (true) {
+        //mic_leitura(&valor_mic);
+        //printf("mic: %.2f\n", valor_mic*(3.3/4096));
+        joystick_captura();
+        normalizar_joystick();
+        //printf("X: %d\n", get_joystick_x_norm());
+        //printf("Y: %d\n", get_joystick_y_norm());
+        //printf("\n");
+        printf("Botao: %d\n", get_botao());
+        
+        npClear();
+        npWrite(); // Escreve os dados nos LEDs.
+        sleep_ms(1);
+
+        switch (get_direcao()) {
+            // Direções principais
+            case CIMA1: display_sprite(cima1); break;
+            case CIMA2: display_sprite(cima2); break;
+            case BAIXO1: display_sprite(bai1); break;
+            case BAIXO2: display_sprite(bai2); break;
+            case DIREITA1: display_sprite(dir1); break;
+            case DIREITA2: display_sprite(dir2); break;
+            case ESQUERDA1: display_sprite(esq1); break;
+            case ESQUERDA2: display_sprite(esq2); break;
+        
+            // Diagonais
+            case DIAG_CIMA_DIREITA1: display_sprite(dircim1); break;
+            case DIAG_CIMA_DIREITA2: display_sprite(dircim2); break;
+            case DIAG_CIMA_ESQUERDA1: display_sprite(esqcim1); break;
+            case DIAG_CIMA_ESQUERDA2: display_sprite(esqcim2); break;
+            case DIAG_BAIXO_DIREITA1: display_sprite(dirbax1); break;
+            case DIAG_BAIXO_DIREITA2: display_sprite(dirbax2); break;
+            case DIAG_BAIXO_ESQUERDA1: display_sprite(esqbax1); break;
+            case DIAG_BAIXO_ESQUERDA2: display_sprite(esqbax2); break;
+        
+            case NEUTRO: display_sprite(neutro); break;
+        }
+        npWrite();
+        
+    }
+}
